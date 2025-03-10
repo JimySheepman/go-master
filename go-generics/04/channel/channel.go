a837c94c2fbdefc1b297733a62070e26de5652fa
JimySheepman <y.alikoyuncu@gmail.com>
2025-03-10 10:45:06 +0300

    add generic tutorial

---
+++ go-generics/04/channel/channel.go
@@ -0,0 +1,34 @@
+package channel
+
+import "sync/atomic"
+
+type Channel[T any] struct {
+	ch       chan T
+	sends    atomic.Uint64
+	receives atomic.Uint64
+}
+
+func New[T any](length int) *Channel[T] {
+	return &Channel[T]{
+		ch: make(chan T, length),
+	}
+}
+
+func (c *Channel[T]) Send(v T) {
+	c.ch <- v
+	c.sends.Add(1)
+}
+
+func (c *Channel[T]) Receive() T {
+	v := <-c.ch
+	c.receives.Add(1)
+	return v
+}
+
+func (c *Channel[T]) Sends() uint64 {
+	return c.sends.Load()
+}
+
+func (c *Channel[T]) Receives() uint64 {
+	return c.receives.Load()
+}
