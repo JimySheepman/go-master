6c2161f221ec0349d517cb0c41edf5c65cc692cb
JimySheepman <y.alikoyuncu@gmail.com>
2025-03-10 16:04:33 +0300

    add generic tutorial

---
+++ go-generics/04/intish/intish_test.go
@@ -0,0 +1,32 @@
+package intish_test
+
+import (
+	"go-generics/04/intish"
+	"testing"
+)
+
+type MyInt int
+
+func TestIsPositive_IsTrueFor1(t *testing.T) {
+	t.Parallel()
+	input := MyInt(1)
+	if !intish.IsPositive(input) {
+		t.Errorf("IsPositive(1): want true, got false")
+	}
+}
+
+func TestIsPositive_IsFalseForNegative1(t *testing.T) {
+	t.Parallel()
+	input := MyInt(-1)
+	if intish.IsPositive(input) {
+		t.Errorf("IsPositive(-1): want false, got true")
+	}
+}
+
+func TestIsPositive_IsFalseForZero(t *testing.T) {
+	t.Parallel()
+	input := MyInt(0)
+	if intish.IsPositive(input) {
+		t.Errorf("IsPositive(0): want false, got true")
+	}
+}
